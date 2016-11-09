f65dbc03c0d613a2bf0c3e0346eb3917d0d0a7a8
KMS <KMS>
2016-11-09 22:05:05 +0530

    First Code Commit

---
+++ app/scripts/controllers/RoleEntityCtrl.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('apApp.controllers', []).
+	controller('RoleEntityCtrl', function($scope, $rootScope)
+	{
+		console.log('called');
+		$scope.list = [
+			  {
+			    "id": 1,
+			    "title": "node1",
+			    "nodes": [
+			      {
+			        "id": 11,
+			        "title": "node1.1",
+			        "nodes": [
+			          {
+			            "id": 111,
+			            "title": "node1.1.1",
+			            "nodes": []
+			          }
+			        ]
+			      },
+			      {
+			        "id": 12,
+			        "title": "node1.2",
+			        "nodes": []
+			      }
+			    ]
+			  },
+			  {
+			    "id": 2,
+			    "title": "node2",
+			    "nodrop": true,
+			    "nodes": [
+			      {
+			        "id": 21,
+			        "title": "node2.1",
+			        "nodes": []
+			      },
+			      {
+			        "id": 22,
+			        "title": "node2.2",
+			        "nodes": []
+			      }
+			    ]
+			  },
+			  {
+			    "id": 3,
+			    "title": "node3",
+			    "nodes": [
+			      {
+			        "id": 31,
+			        "title": "node3.1",
+			        "nodes": []
+			      }
+			    ]
+			  }
+			]
+	});
